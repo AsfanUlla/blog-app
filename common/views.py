e44e3360cbe52c78cffac5f62f5c689cc0c0d24c
AsfanUlla <asfanulla@gmail.com>
2021-10-27 20:13:00 +0530

    removed unnessaary js and css and added article tags to meta keyword tag

--- common/views.py
+++ common/views.py
@@ -186,7 +186,7 @@ async def redirect(request: Request, exc: RediectException):
 class CurrentHost(BaseHTTPMiddleware):
     async def __call__(self, scope, receive, send) -> None:
         request = Request(scope, receive)
-        request.state.current_host = request.base_url.hostname.capitalize()
+        request.state.current_host = request.base_url.hostname.split('.')[0].capitalize()
         request.state.current_host_url = str(request.base_url).strip("/")
         if Config().ENV != "LOCAL":
             await MongoInterface.find_or_404(
